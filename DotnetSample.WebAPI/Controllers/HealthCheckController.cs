79d944c65ae2d7ffd23926de701f7cc1b4e2ecb7
Rafael Goncalves <rafael.goncalves@des.qld.gov.au>
2022-01-07 09:54:13 +1000

    First commit

---
+++ DotnetSample.WebAPI/Controllers/HealthCheckController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace DotnetSample.WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthCheckController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public HealthCheckController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet]
+    public async Task<HealthCheckDto> Get()
+    {
+        var date = await GetCurrentDate();
+        return new HealthCheckDto { Date = date };
+    }
+
+    private async Task<DateTime> GetCurrentDate()
+    {
+        var connectionString = _configuration.GetConnectionString("DotnetSample");
+
+        var connection = new NpgsqlConnection(connectionString);
+        await connection.OpenAsync();
+
+        DateTime date = default;
+        await using var cmd = new NpgsqlCommand("SELECT now()", connection);
+        await using var reader = await cmd.ExecuteReaderAsync();
+        while (await reader.ReadAsync())
+        {
+            date = reader.GetDateTime(0);
+        }
+        return date;
+    }
+}
+
+public class HealthCheckDto
+{
+    public DateTime Date { get; set;  }
+}
